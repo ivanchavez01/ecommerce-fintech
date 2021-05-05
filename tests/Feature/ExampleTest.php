4141afdcddf91923bdb1fe75ba2c02bc67ecfc51
Ivan Chavez <ichavez@redgirasol.com>
2021-05-05 16:10:42 -0700

    Continous integration

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,12 +3,6 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Soft\Ecommerce\Application\Product;
-use Soft\Ecommerce\Infrastructure\ElasticSearchProductRepository;
-use Soft\Fintech\Wallet\Application\Wallet;
-use Soft\Fintech\Wallet\Domain\Entities\Applicant;
-use Soft\Fintech\Wallet\Domain\Entities\Person;
-use Soft\Fintech\Wallet\Domain\Entities\PhysicalPerson;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -20,22 +14,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        /*$product = new Product(new ElasticSearchProductRepository());
-        $product->find(189);*/
-
-
-
-        Wallet::create(
-            new PhysicalPerson(
-              "Ivan",
-                "Ernesto",
-                "Chavez",
-                "Sanchez",
-                "CASI9001HSRHNV04",
-                "CASI9001KL0"
-            )
-        );
-
         $response = $this->get('/');
 
         $response->assertStatus(200);
